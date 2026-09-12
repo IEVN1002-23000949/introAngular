4466b9ed86452ee9164189ce3cf0a63fef84ce60
Carlos <83031@alumnos.utleon.edu.mx>
2026-09-11 20:45:41 -0600

    Heroes HTML-11/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,17 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('introAngular');
+  titulo:string = "Carlos Ibarra"
+  duplicaNumero(num:number):number{
+    return num*2
+  }
+  
+  pelicula={
+    titulo:"El Padrino",
+    anio:1920,
+    genero:"Crimen/Drama",
+    fechaLanzamiento:new Date(),
+    precio:345
+  }
+  
 }
