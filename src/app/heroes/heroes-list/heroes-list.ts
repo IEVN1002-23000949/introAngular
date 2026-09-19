69b7466710f556189f5cd426aae9003ca4d928e1
Carlos <83031@alumnos.utleon.edu.mx>
2026-09-18 20:51:53 -0600

    ngModule-18/09/2026

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -6,4 +6,46 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+imageWidth:number=40;
+imageMargin:number=2;
+muestraImage:boolean=true;
+listFilter:string='';
+
+showImage():void{
+  this.muestraImage=!this.muestraImage
+}
+
+
+heroes:any[]=[
+{
+  imagen:"https://dragonball-api.com/transformaciones/gogeta.webp",
+  nombre:"Gogeta",
+  descripcion:"Fusion",
+  race:"Saiyan",
+  ki:150000
+},
+{
+  imagen:"https://dragonball-api.com/characters/Jiren.webp",
+  nombre:"Jiren",
+  descripcion:"Fuerza del Orden",
+  race:"Jiren Race",
+  ki:120000
+},
+{
+  imagen:"https://dragonball-api.com/characters/Beerus_DBS_Broly_Artwork.webp",
+  nombre:"Bills",
+  descripcion:"Dios de la Destrucccion",
+  race:"God",
+  ki:1000000
+},
+{
+  imagen:"https://dragonball-api.com/characters/Raditz_artwork_Dokkan.webp",
+  nombre:"Raditz",
+  descripcion:"Hijo malo",
+  race:"Saiyan",
+  ki:1500
+}
+]
+
+}
